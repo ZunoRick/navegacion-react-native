0a8040daeee2e7fb9bc59e6676a7b14c09336125
Ricardo <zunosan.ricardo506@gmail.com>
2023-02-10 20:00:17 -0600

    Agregando Drawer Navigation

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,6 +1,6 @@
-import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
 import { Button, Text, TouchableOpacity, View } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack'
 import { styles } from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any>{
@@ -8,6 +8,7 @@ interface Props extends StackScreenProps<any, any>{
 }
 
 export const Pagina1Screen = ( { navigation }:Props ) => {
+  
   return (
     <View style={ styles.globalMargin }>
       <Text style={ styles.title }>Pagina1Screen</Text>
@@ -17,7 +18,11 @@ export const Pagina1Screen = ( { navigation }:Props ) => {
         onPress={ () => navigation.navigate('Pagina2Screen') }
       />
 
-      <Text>Navegar con argumentos</Text>
+      <Text style={{
+        marginVertical:20,
+        fontSize: 20,
+        marginLeft: 5
+      }}>Navegar con argumentos</Text>
       <View style={{
         flexDirection: 'row'
       }}>
